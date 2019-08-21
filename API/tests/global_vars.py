2d2ffd37caf512559665a99082a82e46a3a3a901
Rob Baynes <rbaynes@media.mit.edu>
2019-08-21 11:05:51 -0400

    update to new CC.  tests pass

--- API/tests/global_vars.py
+++ API/tests/global_vars.py
@@ -2,7 +2,7 @@
 class global_vars:
     user_token = None
     device_uuid = None
-    ACE4_device_uuid = "EDU-D41F7C6-f4-5e-ab-59-ac-e3"
+    ACE4_device_uuid = "EDU-4348119F-f4-5e-ab-6b-a2-31"
     recipe_uuid = "e6085be7-d496-43cc-8bd3-3a40a79e854e" # get growing: basil
     user_email_address = None
     user_username = None
